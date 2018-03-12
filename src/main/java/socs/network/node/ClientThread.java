85e504688a81f80e938f6524d86c79e162c89f04
kevinofsydney <kevinofsydney@gmail.com>
2018-03-11 20:03:56 -0400

    moved the sendLSAinit function to ClientThread

--- src/main/java/socs/network/node/ClientThread.java
+++ src/main/java/socs/network/node/ClientThread.java
@@ -2,8 +2,10 @@ package socs.network.node;
 
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
+import socs.network.message.LSA;
 import socs.network.message.SOSPFPacket;
 
 public class ClientThread extends Thread {
@@ -55,9 +57,43 @@ public class ClientThread extends Thread {
 			}
 			
 			// send HELLO again
-			outputStream.writeObject(message);	
-			
-		} catch (Exception e) {
+			outputStream.writeObject(message);
+
+            // Called when a connection status becomes TWO_WAY
+            // Create the link description (LD), create a link state advertisement (LSA), and add the LD to the LSA
+            LSA initLSA = new LSA();
+            initLSA.lsaSeqNumber = myRouter.lsaSeqNum_counter++;
+            initLSA.linkStateID = myRouter.rd.simulatedIPAddress;
+
+            // Add all of the router's links to the LSA
+            for (Link link : myRouter.ports) {
+                initLSA.addLink(link.router2.simulatedIPAddress, link.router2.processPortNumber, link.weight);
+            }
+
+            // Add the link state advertisement to the link state database
+            myRouter.lsd.addLSA(initLSA);
+
+            // For each active TWO_WAY connection, create a link state packet (LSP) and send it
+            for (Link link : myRouter.ports) {
+                if (link.router2.status == RouterStatus.TWO_WAY) {
+                    SOSPFPacket LSP = new SOSPFPacket();
+                    LSP.srcProcessIP = link.router1.processIPAddress;
+                    LSP.srcProcessPort = link.router1.processPortNumber;
+                    LSP.srcIP = link.router1.simulatedIPAddress;
+                    LSP.dstIP = link.router2.simulatedIPAddress;
+                    LSP.sospfType = 1;
+                    LSP.routerID = link.router1.simulatedIPAddress;
+                    //LSP.neighborID = link.router2.simulatedIPAddress;
+                    LSP.lsaArray.add(initLSA);
+
+                    // Send the packet to each neighbour
+                    // outputStream.writeObject(LSP); I think you know more about this one that I do Toni, sorry bro
+                }
+            }
+
+
+
+        } catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			try {
@@ -69,4 +105,6 @@ public class ClientThread extends Thread {
 			}
 		}
 	}
+
+
 }
--- src/main/java/socs/network/node/Router.java
+++ src/main/java/socs/network/node/Router.java
@@ -14,10 +14,10 @@ import java.util.List;
 
 public class Router {
 
-	protected LinkStateDatabase lsd;
+	public LinkStateDatabase lsd;
 	RouterDescription rd = new RouterDescription();
 	public List<Link> ports = new LinkedList<Link>();
-	private int lsaSeqNum_counter = 0;
+	public int lsaSeqNum_counter = 0;
 
 	public Router(Configuration config) {
 
@@ -129,31 +129,6 @@ public class Router {
 			myClientThread.start();
 		}
 	}
-
-	// Called when attach and start commands are run
-	private void sendLSAInit () {
-        // Create the LSA
-	    LSA initLSA = new LSA();
-        initLSA.lsaSeqNumber = lsaSeqNum_counter++;
-        initLSA.linkStateID = rd.simulatedIPAddress;
-
-        // Add all of the router's links to the LSA
-        for (Link link : ports) {
-            initLSA.addLink(link.router2.simulatedIPAddress, link.router2.processPortNumber, link.weight);
-        }
-
-        // Add the LSA to the LSD
-        lsd.addLSA(initLSA);
-
-        // For each active TWO_WAY connection, create a link state packet (LSP) and send it
-
-        for (Link link : ports) {
-            if (link.router2.status == RouterStatus.TWO_WAY) {
-                SOSPFPacket LSP = new SOSPFPacket();
-            }
-
-        }
-    }
 
     // Called when an LSA packet needs to be forwarded
     /*private void sendLSAUpdate {
